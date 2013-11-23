4ace7323e455a640580a55097084616342cc9a50
DoterSoJet <python_django@mail.ru>
2013-11-24 00:25:51 +0300

    ticket:1 result

--- cups_t1/app_t1/tests.py
+++ cups_t1/app_t1/tests.py
@@ -1,9 +1,12 @@
 from django.test import TestCase
 from django.http import HttpRequest
 from app_t1.views import contact_page
+from app_t1.models import Contacts
 # Create your tests here.
 class Ticket1Test(TestCase):
     def test_contact_page(self):
+        con=Contacts(name='kolian')
+        con.save()
         request=HttpRequest()
         resp=contact_page(request)
         self.assertEqual(resp.status_code,200)
--- cups_t1/app_t1/views.py
+++ cups_t1/app_t1/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,render_to_response
+from app_t1.models import Contacts
 # Create your views here.
+def contact_page(request):
+    my_contact=Contacts.objects.get(name='kolian')
+    return render_to_response('contact.html',{'my_contact':my_contact})
