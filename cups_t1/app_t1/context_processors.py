8bf37d641639e46487c350b04465f258e82ad643
DoterSoJet <python_django@mail.ru>
2013-11-25 21:59:47 +0300

    ticket:4 with some changes from ticket1

--- cups_t1/app_t1/context_processors.py
+++ cups_t1/app_t1/context_processors.py
@@ -1,9 +1,9 @@
 from django.conf import settings
 
 def settings_context_processor(request):
-    arg_list=[arg for arg in dir(settings) if not arg.startswith('_')]
-    w={}
+    arg_list = [arg for arg in dir(settings) if not arg.startswith('_')]
+    w = {}
     for key in arg_list:
-        w[key]=settings.__getattr__(key)
-    return {'settings':w}
+        w[key] = settings.__getattr__(key)
+    return {'settings': w}
     
