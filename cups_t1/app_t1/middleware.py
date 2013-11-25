635c02ade1e1b758f3f2060bac141b8ff0c11bcf
DoterSoJet <python_django@mail.ru>
2013-11-25 21:13:17 +0300

    ticket:3 middleware with some changes from ticket1

--- cups_t1/app_t1/middleware.py
+++ cups_t1/app_t1/middleware.py
@@ -1,10 +1,11 @@
-from app_t1.models import Request
 import datetime
+from app_t1.models import Request
 
 class RequestCounting(object):
-    def process_request(self,request):
-        new=Request()
-        new.name=request.META['REMOTE_ADDR']
-        new.time=datetime.datetime.now()
+
+    def process_request(self, request):
+        new = Request()
+        new.name = request.META['REMOTE_ADDR']
+        new.time = datetime.datetime.now()
         new.save()
         return None
--- cups_t1/app_t1/tests.py
+++ cups_t1/app_t1/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.http import HttpRequest
 from app_t1.views import contact_page
-from app_t1.models import Contacts,Request
+from app_t1.models import Contacts, Request
 from app_t1.middleware import RequestCounting
 
 # Create your tests here.
@@ -9,20 +9,23 @@ from app_t1.middleware import RequestCounting
 class Ticket1Test(TestCase):
 
     def test_contact_page(self):
-        con=Contacts(name='kolian')
+        con = Contacts(name = 'kolian',skype = 'kolian1611')
         con.save()
-        request=HttpRequest()
-        resp=contact_page(request)
-        self.assertEqual(resp.status_code,200)
+        request = HttpRequest()
+        resp = contact_page(request)
+        self.assertEqual(resp.status_code, 200)
+        self.assertFalse(resp.content.find('kolian1623') > 0)
+        self.assertTrue(resp.content.find('kolian1611') > 0)
         
 class MiddlewareTest(TestCase):
+
     def setUp(self):
-        self.md=RequestCounting()
-        self.request=HttpRequest()
-        self.request.META['REMOTE_ADDR']='1.1.1.1'
-        self.request.session={}
+        self.md = RequestCounting()
+        self.request = HttpRequest()
+        self.request.META['REMOTE_ADDR'] = '1.1.1.1'
+        self.request.session = {}
         
     def test_middleware(self):
-        self.assertEqual(self.md.process_request(self.request),None)
-        test_addr=Request.objects.get(name='1.1.1.1')
-        self.assertEqual(test_addr.name,'1.1.1.1')
+        self.assertEqual(self.md.process_request(self.request), None)
+        test_addr = Request.objects.get(name = '1.1.1.1')
+        self.assertEqual(test_addr.name, '1.1.1.1')
