247151f10d86c2e94c13b8295065e2171d921e52
DoterSoJet <python_django@mail.ru>
2013-11-25 20:28:08 +0300

    ticket:1 without mistakes v1

--- cups_t1/app_t1/tests.py
+++ cups_t1/app_t1/tests.py
@@ -4,10 +4,12 @@ from app_t1.views import contact_page
 from app_t1.models import Contacts
 # Create your tests here.
 class Ticket1Test(TestCase):
+
     def test_contact_page(self):
-        con=Contacts(name='kolian')
+        con = Contacts(name = 'kolian',skype = 'kolian1611')
         con.save()
-        request=HttpRequest()
-        resp=contact_page(request)
+        request = HttpRequest()
+        resp = contact_page(request)
         self.assertEqual(resp.status_code,200)
-        
+        self.assertFalse(resp.content.find('kolian1623') > 0)
+        self.assertTrue(resp.content.find('kolian1611') > 0)
