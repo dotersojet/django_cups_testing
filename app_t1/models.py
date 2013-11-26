790ef02e49a74961999ad80106e60cc0181c26c4
dotersojet <python_django@mail.ru>
2013-11-26 21:46:15 +0200

    Update models.py

--- app_t1/models.py
+++ app_t1/models.py
@@ -5,7 +5,7 @@ class Contacts(models.Model):
     name = models.CharField(max_length = 20, blank = True, null = True)
     last_name = models.CharField(max_length = 20, blank = True, null = True)
     date_of_birth = models.DateField(blank = True, null = True)
-    last_name = models.CharField(max_length = 20, blank = True, null = True)
+    bio=models.TextField(blank = True, null = True) 
     email = models.EmailField(blank = True, null = True)
     jabber = models.CharField(max_length = 20, blank = True, null = True)
     skype = models.CharField(max_length = 20, blank = True, null = True)
