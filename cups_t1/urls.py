6469dd1d77fe96eacb9262edab57e7c6d20f741d
dotersojet <python_django@mail.ru>
2013-11-27 00:09:14 +0200

    Update urls.py

--- cups_t1/urls.py
+++ cups_t1/urls.py
@@ -9,5 +9,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$','app_t1.views.contact_page',name='contact_page'), 
+    url(r'^$', 'app_t1.views.contact_page', name = 'contact_page'), 
 )
